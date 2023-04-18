f662a0c06d12c23067ee19a810f16603977f50a9
Edd Crossman <eddcrossman@hotmail.com>
2023-04-18 11:06:58 +0100

    added Block class for scenery

--- main.py
+++ main.py
@@ -84,6 +84,14 @@ class Object(pygame.sprite.Sprite):
     def draw(self, win):
         win.blit(self.image, (self.rect.x, self.rect.y))
 
+class Block(Object):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size, size)
+        block = get_block(size)
+        self.image.blit(block, (0, 0))
+        self.mask = pygame.mask.from_surface
+
+
 def flip(sprites):
     return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
 
@@ -111,6 +119,13 @@ def load_sprite_sheets(dir1, dir2, width, height, direction=False):
 
     return all_sprites
 
+def get_block(size):
+    path = join("assets", "Terrain", "Terrain.png")
+    image = pygame.image.load(path).convert_alpha()
+    surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
+    rect = pygame.Rect(96, 0, size, size)
+    surface.blit(image, (0, 0), rect)
+    return pygame.transform.scale2x(surface)
 
 
 def get_background(name):
